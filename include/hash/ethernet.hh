a0530338b1fca3feff4fe9f38a3f5ceabc17ce7f
Julian Stecklina <js@alien8.de>
2013-02-20 00:06:38 +0100

    Fix potential bug. Compiler thought hash() only depends on pointer to MAC address.

--- include/hash/ethernet.hh
+++ include/hash/ethernet.hh
@@ -3,16 +3,19 @@
 #include <header/ethernet.hh>
 #include <util.hh>
 
+#include <cstdio>
 namespace Ethernet {
 
   // Hash a MAC address by creatively using CRC32.
   static inline uint32_t hash(Address const &addr) {
-    uint32_t r1, r2;
-    asm ("crc32l  (%2), %0\n"
-         "crc32l 2(%2), %1\n"
-         : "=r" (r1), "=r" (r2)
-         : "r" (addr.byte));
-    return r1 ^ r2;
+    uint32_t r1;
+    asm ("crc32w  (%1), %0\n"
+         "crc32l 2(%1), %0\n"
+         : "=r" (r1)
+           // Need reference to content of addr otherwise the compiler
+           // thinks we only depend on the poiner value.
+         : "r" (addr.byte), "m" (addr));
+    return r1;
   }
 
 }
