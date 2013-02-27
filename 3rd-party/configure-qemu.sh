35860f1b89b75170ce83ea9cb5af08c6beac27e9
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2013-02-27 13:14:52 +0100

    Minimal qemu-kvm configuration

--- 3rd-party/configure-qemu.sh
+++ 3rd-party/configure-qemu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd qemu-kvm && ./configure --disable-virtfs --disable-seccomp --disable-guest-agent --disable-usb-redir --disable-smartcard --disable-libiscsi --disable-spice --disable-bluez --disable-glusterfs --disable-xen --disable-brlapi --disable-vnc --target-list=x86_64-softmmu
+cd qemu-kvm && ./configure --disable-virtfs --disable-seccomp --disable-guest-agent --disable-usb-redir --disable-smartcard --disable-libiscsi --disable-spice --disable-bluez --disable-glusterfs --disable-xen --disable-brlapi --disable-vnc --disable-slirp --disable-opengl --disable-curl --disable-docs --disable-rbd --disable-pie --audio-drv-list= --audio-card-list= --disable-strip --target-list=x86_64-softmmu
