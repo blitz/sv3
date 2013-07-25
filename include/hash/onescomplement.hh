61ba335a2e7629bd26fc847cd7e604fc7bee4542
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2013-07-25 16:06:59 +0200

    Added license headers.

--- include/hash/onescomplement.hh
+++ include/hash/onescomplement.hh
@@ -1,3 +1,17 @@
+// Copyright (C) 2013, Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
+// Economic rights: Technische Universitaet Dresden (Germany)
+
+// This file is part of sv3.
+
+// sv3 is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+
+// sv3 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License version 2 for more details.
+
 // One's complement checksum
 
 #pragma once
